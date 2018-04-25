0ac4a5202f90979c074addf77237aa26c5835d80
Sujay Hegde <sujay@localhost.localdomain>
2018-04-25 16:21:49 +0530

    uploaded

---
+++ 10_Swings.java
@@ -0,0 +1,93 @@
+import java.awt.*;
+import java.awt.event.*;
+import java.io.*;
+import javax.swing.*;
+
+public class EmployeeDataEntry {
+	private static Color black;
+
+	public static void main(String[] args) 
+	{
+		int i,j;
+		
+		JFrame frameobj = new JFrame();  				//creating frame
+		frameobj.setSize(500, 500);  					//declaring frame size
+		frameobj.setLayout( new GridLayout(5,2) );		//layout is set to the frame created
+		
+		JPanel p[] = new JPanel[10];		
+		for( i =0; i < 10; i++)
+			p[i] = new JPanel();
+		
+		String words[]  = new String[]{ "NAME", "ID", "DOJ", "DOB" };
+		JLabel l[] = new JLabel[4];
+		for( i = 0; i < 4; i++)
+			l[i] = new JLabel( words[i] );
+	
+		JTextField[] f = new JTextField[4];	
+		for( i = 0; i < 4; i++)
+		{	
+			f[i] = new JTextField();
+			f[i].setPreferredSize(new Dimension(200,30));
+		}	
+
+		JButton b1 = new JButton("SUBMIT");
+		JButton b2 = new JButton("RESET");
+		
+		b1.addActionListener(new ActionListener()
+		{
+			
+			@Override
+			public void actionPerformed(ActionEvent e) 
+			{
+				
+				File fileobj=new File("./EmployeeData.txt");
+				try 
+				{
+					FileWriter fw=new FileWriter(fileobj.getAbsoluteFile(),true);
+					
+					String details = "";
+					for(int i = 0; i <4; i++)
+						details += l[i].getText() + " " +  f[i].getText() + "\n";
+				
+					System.out.println(details);
+					fw.write(details);
+					fw.close();
+				} 
+				catch (IOException e1) 
+				{
+					e1.printStackTrace();
+				}	
+			}
+		});
+		
+		b2.addActionListener(new ActionListener() 
+		{
+			@Override
+			public void actionPerformed(ActionEvent e)
+			{
+				for( int i = 0; i < 4; i++)
+					f[i].setText(null);
+			}
+		});
+		
+		i = 0; j = 0;
+		while( i < 8 )
+		{
+			p[i++].add( l[j] );
+			p[i++].add( f[j] );
+			j++;
+		}
+			
+		p[8].add(b1);		p[9].add(b2);
+
+		for( i = 0; i < 4; i++)
+			l[i].setBorder( BorderFactory.createLineBorder(black,10));
+		
+		for( i = 0; i <10; i++)
+			frameobj.add( p[i] );
+	
+		frameobj.setVisible(true);
+	}
+
+}
+
---
+++ 9_Multithreading.java
@@ -0,0 +1,68 @@
+package multithreading;
+
+import java.util.Date;
+
+public class Multithreading implements Runnable{
+
+	int startCount;
+	String name;
+	Thread t;
+	long total;
+	
+	public Multithreading(String name, int startCount)
+	{
+		this.name 		= name;
+		this.startCount = startCount;
+		t = new Thread(this, name);
+		t.start();
+	}
+	
+	public void run()
+	{
+		System.out.println(Thread.currentThread().getName() + " running ...");
+		long sum = 0;
+		for(int i = startCount; i < startCount +10; i++)
+			sum += i;
+		
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		
+		System.out.println("Sum of " + startCount + " to " + startCount+10 + " = " + sum);
+		total += sum;
+		
+	}
+	
+	
+	public static void main(String[] args) 
+	{
+		int startCount = 1;
+		Multithreading ob[] = new Multithreading[5];
+		
+		Date start = new Date();
+		for(int i = 0; i < 5; i++)
+		{
+			String tName = "Thread_" + i;			
+			ob[i] = new Multithreading(tName, startCount);
+			startCount += 10;
+		}
+		
+		try{
+			for(int i = 0; i < 5; i++)
+				ob[i].t.join();
+			}
+		catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		Date end = new Date();
+		
+		long diff = end.getTime() - start.getTime();
+		System.out.println("Computation time : " + diff + "ms");
+		
+		
+	}
+
+}
+
